9d6ad4e6255533925759a8a17d9a2c2d13b86548
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-01-07 15:17:57 +0000

    refactored arrays_4

--- arrays_4.rb
+++ arrays_4.rb
@@ -1,6 +1,3 @@
 array = [1, 2, 3, 4, 5]
-sum = 0
 
-array.each { |num| sum += num }
-
-p sum
+p array.sum
--- hashes_4.rb
+++ hashes_4.rb
@@ -4,4 +4,5 @@ new_hash = {}
 hash.each do |key, value|
   new_hash[value.to_s] = key.to_s
 end
+
 p new_hash
